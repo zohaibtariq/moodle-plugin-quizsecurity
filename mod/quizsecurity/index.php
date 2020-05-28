80be43fc61413ca276aff95ef02c5160f05167c0
Zohaib Tariq <se.zohaib@gmail.com>
2020-05-28 12:19:32 +0500

    removed comments and removed some more code so can user review and identify issue qucikly

--- mod/quizsecurity/index.php
+++ mod/quizsecurity/index.php
@@ -1,43 +1,25 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * This script lists all the instances of quiz in a particular course
- *
- * @package    mod_quizsecurity
- * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 
 require_once("../../config.php");
 require_once("../quiz/locallib.php");
 
 $id = required_param('id', PARAM_INT);
 $PAGE->set_url('/mod/quizsecurity/index.php', array('id'=>$id));
+
 if (!$course = $DB->get_record('course', array('id' => $id))) {
     print_error('invalidcourseid');
 }
+
 $coursecontext = context_course::instance($id);
+
 require_login($course);
+
 $PAGE->set_pagelayout('incourse');
 
 $params = array(
     'context' => $coursecontext
 );
+
 $event = \mod_quizsecurity\event\course_module_instance_list_viewed::create($params);
 $event->trigger();
 
--- mod/quizsecurity/mod_form.php
+++ mod/quizsecurity/mod_form.php
@@ -1,65 +1,17 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Defines the quizsecurity module ettings form.
- *
- * @package    mod_quizsecurity
- * @copyright  2006 Jamie Pratt
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-error_log("\n" . 'inside mod_form class', 3, __DIR__ . '/../../moodle.log');
 
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . '/course/moodleform_mod.php');
-require_once($CFG->dirroot . '/mod/quizsecurity/locallib.php');
 
-/**
- * Settings form for the quizsecurity module.
- *
- * @copyright  2006 Jamie Pratt
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 class mod_quizsecurity_mod_form extends moodleform_mod {
-    /** @var array options to be used with date_time_selector fields in the quizsecurity. */
-    public static $datefieldoptions = array('optional' => true);
-
-    protected $_feedbacks;
-    protected static $reviewfields = array(); // Initialised in the constructor.
-
-    /** @var int the max number of attempts allowed in any user or group override on this quizsecurity. */
-    protected $maxattemptsanyoverride = null;
 
     public function __construct($current, $section, $cm, $course) {
-        self::$reviewfields = array(
-            'attempt'          => array('theattempt', 'quizsecurity'),
-            'correctness'      => array('whethercorrect', 'question'),
-            'marks'            => array('marks', 'quizsecurity'),
-            'specificfeedback' => array('specificfeedback', 'question'),
-            'generalfeedback'  => array('generalfeedback', 'question'),
-            'rightanswer'      => array('rightanswer', 'question'),
-            'overallfeedback'  => array('reviewoverallfeedback', 'quizsecurity'),
-        );
         parent::__construct($current, $section, $cm, $course);
     }
 
     protected function definition() {
         global $COURSE, $CFG, $DB, $PAGE;
-        $PAGE->requires->yui_module('moodle-mod_quizsecurity-modform', 'M.mod_quizsecurity.modform.init');
     }
+
 }
